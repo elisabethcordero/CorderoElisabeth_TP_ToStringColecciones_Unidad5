ae9931583d6968e021c13332b1e5ca583eb11179
Elisabeth <124397954+elisabethcordero@users.noreply.github.com>
2026-09-23 17:52:31 -0300

    Bloque 3: clase Producto y Categoria

---
+++ src/entidades/Categoria.java
@@ -0,0 +1,67 @@
+package entidades;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Categoria extends Base {
+
+    private String nombre;
+    private String descripcion;
+    private Set<Producto> productos;
+
+    public Categoria(Long id, String nombre, String descripcion) {
+        super(id);
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.productos = new HashSet<>();
+    }
+
+    // Métodos para manejar la colección
+    public void addProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    public void removeProducto(Producto producto) {
+        productos.remove(producto);
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Set<Producto> getProductos() {
+        return productos;
+    }
+
+    // toString, equals y hashCode
+    @Override
+    public String toString() {
+        return "Categoria{id=" + getId() +
+                ", nombre='" + nombre + "'" +
+                ", descripcion='" + descripcion + "'" +
+                ", cantidadProductos=" + productos.size() + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(nombre, categoria.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
+}
---
+++ src/entidades/Producto.java
@@ -0,0 +1,95 @@
+package entidades;
+
+import java.util.Objects;
+
+public class Producto extends Base {
+
+    private String nombre;
+    private Double precio;
+    private String descripcion;
+    private int stock;
+    private String imagen;
+    private Boolean disponible;
+
+    // Constructor
+    public Producto(Long id, String nombre, Double precio, String descripcion,
+                    int stock, String imagen, Boolean disponible) {
+        super(id);
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.imagen = imagen;
+        this.disponible = disponible;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public Boolean getDisponible() {
+        return disponible;
+    }
+
+    // Setters
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setDisponible(Boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    // toString, equals y hashCode
+    @Override
+    public String toString() {
+        return "Producto{id=" + getId() +
+                ", nombre='" + nombre + "'" +
+                ", descripcion='" + descripcion + "'" +
+                ", precio=$" + precio +
+                ", stock=" + stock +
+                ", disponible=" + disponible + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(nombre, producto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
+}
